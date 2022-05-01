e6c8f9f36aa9ed3359ecd61f0d545c75c79d8c26
takaakimatsuda <takaaki-matsuda@hotmail.co.jp>
2022-05-01 17:36:06 +0900

    ドロップダウンに「ユーザー編集」追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,6 +59,9 @@
                                 </a>
 
 								<div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+									<a class="dropdown-item" href="{{ route('users.edit') }}" >
+                                        ユーザー編集
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         ログアウト
                                     </a>
