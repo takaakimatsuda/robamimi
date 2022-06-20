2b3adcbbdade9937a5e4d2f049be85356e3060bb
takaakimatsuda <takaaki-matsuda@hotmail.co.jp>
2022-06-20 18:38:50 +0900

    Seederの更新

--- database/migrations/2022_06_11_180422_create_notifications_table.php
+++ database/migrations/2022_06_11_180422_create_notifications_table.php
@@ -15,11 +15,14 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id')->nullable(false);
-            $table->integer('comment_id');
-            $table->integer('like_id');
-            $table->timestamp('read_at');
+            $table->integer('user_id')->cascadeOnDelete()->comment('通知を受け取ったユーザーのID')->nullable(false);
+            $table->integer('comment_id')->comment('コメントのID')->nullable();
+            $table->integer('like_id')->comment('いいねのID')->nullable();
+            $table->timestamp('read_at')->comment('通知を読んだ時刻')->nullable();
             $table->timestamps();
+
+			// 削除処理はソフトデリートであることを定義
+			$table->softDeletes();
         });
     }
 
